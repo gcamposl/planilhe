72695e466688723d86bb394e66eb8095ecb62fbc
gcamposl <gcamposlopes@gmail.com>
2024-11-28 22:59:11 -0300

    feat: init routes

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,6 +1,18 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Router represents all routes from API
+type Router struct {
+	URI         string
+	Method      string
+	Function    func(http.ResponseWriter, *http.Request)
+	RequireAuth bool
+}
 
 // Generate returns a new router
 func Generate() *mux.Router {
