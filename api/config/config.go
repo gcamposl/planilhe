06542d6e476315de1f9f8e681c25c2dc8158d89e
gcamposl <gcamposlopes@gmail.com>
2025-01-13 22:42:16 -0300

    feat: created env.example

---
+++ api/config/config.go
@@ -0,0 +1,13 @@
+package config
+
+var (
+	// StringConnection is the connection string to the database
+	ConnectionString = ""
+	// Port is the port where the database is running
+	Port = 0
+)
+
+// Load loads the configuration
+func Load() {
+
+}
