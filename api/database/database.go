b80bdb458049f1089fd3d8e1d30ffb2842830835
gcamposl <gcamposlopes@gmail.com>
2025-01-14 22:54:41 -0300

    feat: created a connection with database

---
+++ api/database/database.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"api/config"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql" // Driver
+)
+
+const (
+	driver = "mysql"
+)
+
+// Connect opens the connection with the database
+func Connect() (*sql.DB, error) {
+	db, err := sql.Open(driver, config.ConnectionString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- api/main.go
+++ api/main.go
@@ -10,8 +10,7 @@ import (
 
 func main() {
 	config.Load()
-	fmt.Println(config.Port)
-	fmt.Printf("api on\n")
 	r := router.Generate()
+	fmt.Printf("listening on port %d", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
