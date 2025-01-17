d468ce25f7edbaa91a273b6b57d5f1d512182450
gcamposl <gcamposlopes@gmail.com>
2025-01-16 22:45:34 -0300

    feat: created user method from insert data

--- api/src/repositories/user.go
+++ api/src/repositories/user.go
@@ -15,6 +15,25 @@ func NewUserRepository(db *sql.DB) *user {
 }
 
 // Create creates a new user
-func (u *user) Create(user models.User) (uint64, error) {
-	return 0, nil
+func (userRepo user) Create(user models.User) (uint64, error) {
+	stmt, err := userRepo.db.Prepare(
+		"insert into users (name, nick, email, password) values (?, ?, ?, ?)",
+	)
+
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(user.Name, user.Nick, user.Email, user.Password)
+	if err != nil {
+		return 0, err
+	}
+
+	lastId, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(lastId), nil
 }
