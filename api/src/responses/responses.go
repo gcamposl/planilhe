cd3f005af7335ade0b55cb493525fca5b3fab703
gcamposl <gcamposlopes@gmail.com>
2025-01-22 00:25:40 -0300

    feat: create a new response file

---
+++ api/src/responses/responses.go
@@ -0,0 +1,20 @@
+package responses
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// JSON returns a JSON response to the request
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Error() {
+
+}
