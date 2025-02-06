c0ca8d75566ee7618a5be89339b2d49bab0b0031
gcamposl <gcamposlopes@gmail.com>
2025-02-05 21:36:03 -0300

    feat: password hashing implementation by  bcrypt

---
+++ api/internal/security/security.go
@@ -0,0 +1,13 @@
+package security
+
+import "golang.org/x/crypto/bcrypt"
+
+// Hash generates a hash from a password
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+// VerifyPassword checks if the password is correct
+func VerifyPassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
