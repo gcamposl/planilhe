d50c2400da08c61437027a29a153d09d5946dd69
gcamposl <gcamposlopes@gmail.com>
2025-02-12 20:19:14 -0300

    feat: created token generation

---
+++ api/internal/auth/token.go
@@ -0,0 +1,18 @@
+package auth
+
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// CreateToken create a new token with user permissions
+func CreateToken(userID uint64) (string, error) {
+	permissions := jwt.MapClaims{}
+	permissions["authorized"] = true
+	permissions["exp"] = time.Now().Add(time.Hour * 6).Unix()
+	permissions["userID"] = userID
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
+
+	return token.SignedString([]byte("Secret")) // secret
+}
