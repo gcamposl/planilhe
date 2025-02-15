ba6846fa51a55e03c7b1f6e2af1a55ecc030ec80
gcamposl <gcamposlopes@gmail.com>
2025-02-15 00:06:55 -0300

    fix

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -11,9 +11,6 @@ import (
 func main() {
 	config.Load()
 	r := router.Generate()
-
-	fmt.Println(config.SecretKey)
-
 	fmt.Printf("listening on port %d\n", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
